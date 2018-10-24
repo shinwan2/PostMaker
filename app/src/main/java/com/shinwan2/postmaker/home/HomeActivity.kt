5fe21a0550b0c1afeaa34776f17834fd9ec9c82b
shinwan2 <shinwan2@gmail.com>
2018-10-24 13:32:31 +0700

    Create layout for HomeActivity
    - Add icons
    - Add DrawerLayout
    - Add FAB

--- app/src/main/java/com/shinwan2/postmaker/home/HomeActivity.kt
+++ app/src/main/java/com/shinwan2/postmaker/home/HomeActivity.kt
@@ -1,7 +1,18 @@
 package com.shinwan2.postmaker.home
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+import com.shinwan2.postmaker.R
 
 class HomeActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
 
+    companion object {
+        fun intent(context: Context) = Intent(context, HomeActivity::class.java)
+    }
 }
