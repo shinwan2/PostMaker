810ed2a66d55ddd12663243538486b856ce652ab
shinwan2 <shinwan2@gmail.com>
2018-10-24 21:46:39 +0700

    Add logic to handle close and open drawer

--- app/src/main/java/com/shinwan2/postmaker/home/HomeActivity.kt
+++ app/src/main/java/com/shinwan2/postmaker/home/HomeActivity.kt
@@ -3,13 +3,56 @@ package com.shinwan2.postmaker.home
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.shinwan2.postmaker.R
+import kotlinx.android.synthetic.main.activity_home.drawerLayout
+import kotlinx.android.synthetic.main.activity_home.topToolbar
 
-class HomeActivity : FragmentActivity() {
+class HomeActivity : AppCompatActivity() {
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(topToolbar)
+        supportActionBar!!.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeButtonEnabled(true)
+        }
+
+        drawerToggle = ActionBarDrawerToggle(
+            this, drawerLayout, topToolbar,
+            R.string.home_drawer_contentdescription_open,
+            R.string.home_drawer_contentdescription_close
+        )
+        drawerLayout.addDrawerListener(drawerToggle)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
