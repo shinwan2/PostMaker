2e43f03e7bf0b34bd7017e87c4302596422c2a8a
shinwan2 <shinwan2@gmail.com>
2018-10-25 23:35:58 +0700

    Implement editUserProfile in UserService

--- core/src/main/java/com/shinwan2/postmaker/core/FirebaseUserService.kt
+++ core/src/main/java/com/shinwan2/postmaker/core/FirebaseUserService.kt
@@ -7,7 +7,9 @@ import com.google.firebase.database.ValueEventListener
 import com.shinwan2.postmaker.core.model.FirebaseUserModel
 import com.shinwan2.postmaker.domain.UserService
 import com.shinwan2.postmaker.domain.auth.AuthenticationService
+import com.shinwan2.postmaker.domain.model.EditUserProfileRequest
 import com.shinwan2.postmaker.domain.model.User
+import io.reactivex.Completable
 import io.reactivex.Single
 import timber.log.Timber
 
@@ -17,7 +19,12 @@ class FirebaseUserService(
 ) : UserService {
     override fun getUser(userId: String?): Single<User> {
         return Single.create<User> { emitter ->
-            val nonNullUserId: String = userId ?: authenticationService.userId!!
+            val nonNullUserId: String? = userId ?: authenticationService.userId
+            if (nonNullUserId == null) {
+                emitter.onError(IllegalStateException("userId is null"))
+                return@create
+            }
+
             firebaseDatabase.getReference("users/$nonNullUserId")
                 .addListenerForSingleValueEvent(object : ValueEventListener {
                     override fun onCancelled(error: DatabaseError) {
@@ -28,7 +35,7 @@ class FirebaseUserService(
                     override fun onDataChange(snapshot: DataSnapshot) {
                         try {
                             val firebaseUser = snapshot.getValue(FirebaseUserModel::class.java)!!
-                            emitter.onSuccess(firebaseUser.map(nonNullUserId))
+                            emitter.onSuccess(firebaseUser.toUser(nonNullUserId))
                         } catch (e: Exception) {
                             emitter.onError(e)
                         }
@@ -37,7 +44,37 @@ class FirebaseUserService(
         }
     }
 
-    private fun FirebaseUserModel.map(userId: String): User {
+    override fun editUserProfile(request: EditUserProfileRequest): Completable {
+        return Completable.create { emitter ->
+            val nonNullUserId: String? = authenticationService.userId
+            if (nonNullUserId == null) {
+                emitter.onError(IllegalStateException("userId is null"))
+                return@create
+            }
+
+            val requestMap = request.toMap()
+            firebaseDatabase.getReference("users/$nonNullUserId")
+                .updateChildren(requestMap)
+                .addOnSuccessListener {
+                    Timber.d("editUserProfile success")
+                    emitter.onComplete()
+                }
+                .addOnFailureListener {
+                    Timber.w(it, "editUserProfile for ID $nonNullUserId $requestMap")
+                    emitter.onError(it)
+                }
+        }
+    }
+
+    private fun EditUserProfileRequest.toMap(): Map<String, Any> {
+        return mapOf(
+            "displayName" to displayName,
+            "aboutMe" to aboutMe,
+            "photoUrl" to photoUrl
+        )
+    }
+
+    private fun FirebaseUserModel.toUser(userId: String): User {
         return User(
             userId = userId,
             email = checkNotNull(email),
