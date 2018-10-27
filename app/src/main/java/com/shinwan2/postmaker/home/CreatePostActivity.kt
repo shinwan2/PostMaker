39492c0d79ed3b1f1eb6ef465e38f630ea0b9135
shinwan2 <shinwan2@gmail.com>
2018-10-27 16:47:00 +0700

    Add MenuItem send to top toolbar

--- app/src/main/java/com/shinwan2/postmaker/home/CreatePostActivity.kt
+++ app/src/main/java/com/shinwan2/postmaker/home/CreatePostActivity.kt
@@ -4,12 +4,49 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.shinwan2.postmaker.R
+import com.shinwan2.postmaker.util.tintWithColorStateList
+import kotlinx.android.synthetic.main.activity_home.topToolbar
 
 class CreatePostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
+
+        setSupportActionBar(topToolbar)
+        supportActionBar!!.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeButtonEnabled(true)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.create_post, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
+        val menuIds = intArrayOf(R.id.menuSubmitPost)
+        for (menuId in menuIds) {
+            menu.findItem(menuId).also {
+                it.tintWithColorStateList(this, R.color.selector_menu_item_tint_dark_background)
+                it.isEnabled = true // TODO: bind to ViewModel value
+            }
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuSubmitPost -> submitPost()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun submitPost() {
+
     }
 
     companion object {
