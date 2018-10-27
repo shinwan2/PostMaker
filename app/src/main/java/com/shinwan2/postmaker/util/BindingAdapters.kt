175b6db0827b2f006ea1327520544d1e8ef217cf
shinwan2 <shinwan2@gmail.com>
2018-10-27 21:18:01 +0700

    Use BindingAdapter for isVisible property

--- app/src/main/java/com/shinwan2/postmaker/util/BindingAdapters.kt
+++ app/src/main/java/com/shinwan2/postmaker/util/BindingAdapters.kt
@@ -2,8 +2,14 @@ package com.shinwan2.postmaker.util
 
 import android.databinding.BindingAdapter
 import android.support.design.widget.TextInputLayout
+import android.view.View
 
 @BindingAdapter("errorText")
 fun setErrorMessage(view: TextInputLayout, errorMessage: String?) {
     view.error = errorMessage
+}
+
+@BindingAdapter("isVisible")
+fun setVisible(view: View, isVisible: Boolean?) {
+    view.visibility = if (isVisible == true) View.VISIBLE else View.GONE
 }
