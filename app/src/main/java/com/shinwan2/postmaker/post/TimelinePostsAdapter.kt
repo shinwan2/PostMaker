1806333dc2d914158bac9563422e50bf8f255e8f
shinwan2 <shinwan2@gmail.com>
2018-10-28 16:55:39 +0700

    Prettify item_post layout
    - Add TextDrawable for user display name initial character
    - Add delete icon

--- app/src/main/java/com/shinwan2/postmaker/post/TimelinePostsAdapter.kt
+++ app/src/main/java/com/shinwan2/postmaker/post/TimelinePostsAdapter.kt
@@ -1,12 +1,17 @@
 package com.shinwan2.postmaker.post
 
+import android.graphics.Color
 import android.support.v7.recyclerview.extensions.ListAdapter
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.amulyakhare.textdrawable.TextDrawable
+import com.amulyakhare.textdrawable.util.ColorGenerator
 import com.shinwan2.postmaker.databinding.ItemPostBinding
 import com.shinwan2.postmaker.domain.model.Post
+
+private val COLOR_GENERATOR = ColorGenerator.MATERIAL
 
 internal class TimelinePostsAdapter : ListAdapter<Post, PostViewHolder>(PostDiffUtilCallback()) {
 
@@ -26,8 +31,7 @@ internal class TimelinePostsAdapter : ListAdapter<Post, PostViewHolder>(PostDiff
         }
 
         override fun areContentsTheSame(oldItem: Post, newItem: Post): Boolean {
-            // TODO("use display name, text content, timestamp")
-            return oldItem.textContent == newItem.textContent ||
+            return oldItem.textContent == newItem.textContent &&
                 oldItem.createdTimestamp == newItem.createdTimestamp
         }
     }
@@ -39,6 +43,19 @@ internal class PostViewHolder(
 
     fun bind(post: Post) {
         binding.viewModel = post
+
+        val textDrawable = TextDrawable.builder()
+            .beginConfig()
+                .textColor(Color.WHITE)
+                .toUpperCase()
+                .bold()
+            .endConfig()
+            .buildRound(
+                post.user?.displayName?.first().toString(),
+                COLOR_GENERATOR.getColor(post.user?.email)
+            )
+        binding.userPhotoImageView.setImageDrawable(textDrawable)
+
         binding.executePendingBindings()
     }
 }
