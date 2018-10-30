36b53166a4cd8f4f068d70807ce21d56b41b2537
shinwan2 <shinwan2@gmail.com>
2018-10-30 23:23:34 +0700

    Add UI testcases in sign in and sign up screens
    Also refactor to use static imports.

--- app/src/androidTest/java/com/shinwan2/postmaker/auth/SignInActivityTest.kt
+++ app/src/androidTest/java/com/shinwan2/postmaker/auth/SignInActivityTest.kt
@@ -1,13 +1,26 @@
 package com.shinwan2.postmaker.auth
 
 import android.support.test.InstrumentationRegistry
-import android.support.test.espresso.Espresso
-import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.Espresso.closeSoftKeyboard
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.clearText
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.pressImeActionButton
+import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.assertion.ViewAssertions.doesNotExist
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.intent.Intents
 import android.support.test.espresso.intent.matcher.IntentMatchers
 import android.support.test.espresso.intent.rule.IntentsTestRule
+import android.support.test.espresso.matcher.RootMatchers.withDecorView
 import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.isEnabled
+import android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.runner.AndroidJUnit4
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import com.shinwan2.postmaker.DaggerCoreTestComponent
@@ -20,6 +33,7 @@ import com.shinwan2.postmaker.domain.auth.AuthenticationService
 import com.shinwan2.postmaker.home.HomeActivity
 import io.reactivex.Completable
 import io.reactivex.Observable
+import org.hamcrest.CoreMatchers.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -27,6 +41,7 @@ import org.junit.runner.RunWith
 
 private const val VALID_EMAIL = "abcdef@gmail.com"
 private const val VALID_PASSWORD = "123456"
+private const val ERROR = "ERROR"
 
 @RunWith(AndroidJUnit4::class)
 class SignInActivityTest {
@@ -58,19 +73,91 @@ class SignInActivityTest {
     }
 
     @Test
+    fun testInitialState() {
+        intentsTestRule.launchActivity(null)
+
+        verifyButtonDisabled()
+        verifyErrorEmailRequiredGone()
+        verifyErrorPasswordRequiredGone()
+    }
+
+    @Test
+    fun testFillEmailOnly() {
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+
+        verifyErrorEmailRequiredGone()
+        verifyErrorPasswordRequiredGone()
+        verifyButtonDisabled()
+    }
+
+    @Test
+    fun testFillPasswordOnly() {
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.passwordEditText)).perform(typeText(VALID_PASSWORD))
+
+        verifyErrorEmailRequiredGone()
+        verifyErrorPasswordRequiredGone()
+        verifyButtonDisabled()
+    }
+
+    @Test
+    fun testFillEmailAndPassword() {
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText)).perform(typeText(VALID_PASSWORD))
+
+        verifyErrorEmailRequiredGone()
+        verifyErrorPasswordRequiredGone()
+        verifyButtonEnabled()
+    }
+
+    @Test
+    fun testFillEmailAndPasswordThenEmpty() {
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText)).perform(typeText(VALID_PASSWORD))
+
+        onView(withId(R.id.emailEditText)).perform(clearText())
+        onView(withId(R.id.passwordEditText)).perform(clearText())
+
+        verifyErrorEmailRequiredVisible()
+        verifyErrorPasswordRequiredVisible()
+        verifyButtonDisabled()
+    }
+
+    @Test
+    fun testSignInFail() {
+        whenever(authenticationService.signIn(any(), any()))
+            .thenReturn(Completable.error(IllegalStateException(ERROR)))
+
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText))
+            .perform(typeText(VALID_PASSWORD))
+            .perform(pressImeActionButton())
+
+        onView(withText(ERROR))
+            .inRoot(withDecorView(not(intentsTestRule.activity.window.decorView)))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
     fun testSignInSucceeds() {
         whenever(authenticationService.signIn(VALID_EMAIL, VALID_PASSWORD))
             .thenReturn(Completable.complete())
 
         intentsTestRule.launchActivity(null)
 
-        Espresso.onView(ViewMatchers.withId(R.id.emailEditText))
-            .perform(ViewActions.typeText(VALID_EMAIL))
-        Espresso.onView(ViewMatchers.withId(R.id.passwordEditText))
-            .perform(ViewActions.typeText(VALID_PASSWORD))
-        Espresso.closeSoftKeyboard()
-        Espresso.onView(ViewMatchers.withId(R.id.signInButton))
-            .perform(ViewActions.click())
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText)).perform(typeText(VALID_PASSWORD))
+        closeSoftKeyboard()
+        onView(withId(R.id.signInButton)).perform(click())
 
         Intents.intended(IntentMatchers.hasComponent(HomeActivity::class.java.name))
     }
@@ -82,12 +169,40 @@ class SignInActivityTest {
 
         intentsTestRule.launchActivity(null)
 
-        Espresso.onView(ViewMatchers.withId(R.id.emailEditText))
-            .perform(ViewActions.typeText(VALID_EMAIL))
-        Espresso.onView(ViewMatchers.withId(R.id.passwordEditText))
-            .perform(ViewActions.typeText(VALID_PASSWORD))
-            .perform(ViewActions.pressImeActionButton())
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText))
+            .perform(typeText(VALID_PASSWORD))
+            .perform(pressImeActionButton())
 
         Intents.intended(IntentMatchers.hasComponent(HomeActivity::class.java.name))
+    }
+
+    private fun verifyButtonEnabled() {
+        onView(withId(R.id.signInButton)).check(matches(isEnabled()))
+    }
+
+    private fun verifyButtonDisabled() {
+        onView(withId(R.id.signInButton))
+            .check(matches(not(isEnabled())))
+    }
+
+    private fun verifyErrorEmailRequiredVisible() {
+        onView(withText(R.string.signin_email_required_error))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+    }
+
+    private fun verifyErrorEmailRequiredGone() {
+        onView(withText(R.string.signin_email_required_error))
+            .check(doesNotExist())
+    }
+
+    private fun verifyErrorPasswordRequiredVisible() {
+        onView(withText(R.string.signin_password_required_error))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+    }
+
+    private fun verifyErrorPasswordRequiredGone() {
+        onView(withText(R.string.signin_password_required_error))
+            .check(doesNotExist())
     }
 }
--- app/src/androidTest/java/com/shinwan2/postmaker/auth/SignUpActivityTest.kt
+++ app/src/androidTest/java/com/shinwan2/postmaker/auth/SignUpActivityTest.kt
@@ -1,14 +1,27 @@
 package com.shinwan2.postmaker.auth
 
 import android.support.test.InstrumentationRegistry
-import android.support.test.espresso.Espresso
-import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.Espresso.closeSoftKeyboard
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.clearText
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.pressImeActionButton
+import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.intent.Intents
 import android.support.test.espresso.intent.matcher.IntentMatchers
 import android.support.test.espresso.intent.rule.IntentsTestRule
+import android.support.test.espresso.matcher.RootMatchers.withDecorView
 import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.isEnabled
+import android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.filters.MediumTest
 import android.support.test.runner.AndroidJUnit4
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import com.shinwan2.postmaker.DaggerCoreTestComponent
@@ -21,6 +34,8 @@ import com.shinwan2.postmaker.domain.auth.AuthenticationService
 import com.shinwan2.postmaker.home.HomeActivity
 import io.reactivex.Completable
 import io.reactivex.Observable
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Matchers
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -28,6 +43,7 @@ import org.junit.runner.RunWith
 
 private const val VALID_EMAIL = "abcdef@gmail.com"
 private const val VALID_PASSWORD = "123456"
+private const val ERROR = "ERROR"
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
@@ -60,19 +76,95 @@ class SignUpActivityTest {
     }
 
     @Test
+    fun testInitialState() {
+        intentsTestRule.launchActivity(null)
+
+        verifyButtonDisabled()
+        verifyErrorEmailRequiredGone()
+        verifyErrorPasswordRequiredGone()
+    }
+
+    @Test
+    fun testFillEmailOnly() {
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+
+        verifyErrorEmailRequiredGone()
+        verifyErrorPasswordRequiredGone()
+        verifyButtonDisabled()
+    }
+
+    @Test
+    fun testFillPasswordOnly() {
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.passwordEditText)).perform(typeText(VALID_PASSWORD))
+
+        verifyErrorEmailRequiredGone()
+        verifyErrorPasswordRequiredGone()
+        verifyButtonDisabled()
+    }
+
+    @Test
+    fun testFillEmailAndPassword() {
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.emailEditText))
+            .perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText))
+            .perform(typeText(VALID_PASSWORD))
+
+        verifyErrorEmailRequiredGone()
+        verifyErrorPasswordRequiredGone()
+        verifyButtonEnabled()
+    }
+
+    @Test
+    fun testFillEmailAndPasswordThenEmpty() {
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.emailEditText))
+            .perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText))
+            .perform(typeText(VALID_PASSWORD))
+
+        onView(withId(R.id.emailEditText)).perform(clearText())
+        onView(withId(R.id.passwordEditText)).perform(clearText())
+
+        verifyErrorEmailRequiredVisible()
+        verifyErrorPasswordRequiredVisible()
+        verifyButtonDisabled()
+    }
+
+    @Test
+    fun testSignUpFail() {
+        whenever(authenticationService.signUp(any(), any()))
+            .thenReturn(Completable.error(IllegalStateException(ERROR)))
+
+        intentsTestRule.launchActivity(null)
+
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText))
+            .perform(typeText(VALID_PASSWORD))
+            .perform(pressImeActionButton())
+
+        onView(withText(ERROR))
+            .inRoot(withDecorView(not(intentsTestRule.activity.window.decorView)))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
     fun testSignUpSucceeds() {
         whenever(authenticationService.signUp(VALID_EMAIL, VALID_PASSWORD))
             .thenReturn(Completable.complete())
 
         intentsTestRule.launchActivity(null)
 
-        Espresso.onView(ViewMatchers.withId(R.id.emailEditText))
-            .perform(ViewActions.typeText(VALID_EMAIL))
-        Espresso.onView(ViewMatchers.withId(R.id.passwordEditText))
-            .perform(ViewActions.typeText(VALID_PASSWORD))
-        Espresso.closeSoftKeyboard()
-        Espresso.onView(ViewMatchers.withId(R.id.signUpButton))
-            .perform(ViewActions.click())
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText)).perform(typeText(VALID_PASSWORD))
+        closeSoftKeyboard()
+        onView(withId(R.id.signUpButton)).perform(click())
 
         Intents.intended(IntentMatchers.hasComponent(HomeActivity::class.java.name))
     }
@@ -84,12 +176,41 @@ class SignUpActivityTest {
 
         intentsTestRule.launchActivity(null)
 
-        Espresso.onView(ViewMatchers.withId(R.id.emailEditText))
-            .perform(ViewActions.typeText(VALID_EMAIL))
-        Espresso.onView(ViewMatchers.withId(R.id.passwordEditText))
-            .perform(ViewActions.typeText(VALID_PASSWORD))
-            .perform(ViewActions.pressImeActionButton())
+        onView(withId(R.id.emailEditText)).perform(typeText(VALID_EMAIL))
+        onView(withId(R.id.passwordEditText))
+            .perform(typeText(VALID_PASSWORD))
+            .perform(pressImeActionButton())
 
         Intents.intended(IntentMatchers.hasComponent(HomeActivity::class.java.name))
+    }
+
+    private fun verifyButtonEnabled() {
+        onView(withId(R.id.signUpButton))
+            .check(ViewAssertions.matches(isEnabled()))
+    }
+
+    private fun verifyButtonDisabled() {
+        onView(withId(R.id.signUpButton))
+            .check(ViewAssertions.matches(Matchers.not(isEnabled())))
+    }
+
+    private fun verifyErrorEmailRequiredVisible() {
+        onView(withText(R.string.signup_email_required_error))
+            .check(ViewAssertions.matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+    }
+
+    private fun verifyErrorEmailRequiredGone() {
+        onView(withText(R.string.signup_email_required_error))
+            .check(ViewAssertions.doesNotExist())
+    }
+
+    private fun verifyErrorPasswordRequiredVisible() {
+        onView(withText(R.string.signup_password_required_error))
+            .check(ViewAssertions.matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+    }
+
+    private fun verifyErrorPasswordRequiredGone() {
+        onView(withText(R.string.signup_password_required_error))
+            .check(ViewAssertions.doesNotExist())
     }
 }
